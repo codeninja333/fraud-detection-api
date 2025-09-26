158a0894ab1fe3f4931b83f7e3b1a2a4bb67d9da
codeninja333 <tejassaggam1@gmail.com>
2025-09-26 11:14:40 +0530

    Initial Spring boot project setup

---
+++ fraud-detection-api/fraud-detection-api/src/main/java/com/tejas/fraud_detection_api/FraudDetectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.tejas.fraud_detection_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionApiApplication.class, args);
+	}
+
+}
---
+++ fraud-detection-api/fraud-detection-api/src/test/java/com/tejas/fraud_detection_api/FraudDetectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.fraud_detection_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
